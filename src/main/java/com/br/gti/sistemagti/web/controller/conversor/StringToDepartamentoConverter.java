85f79ebe1a84ffaaf19a129216e16da2c66e0ab6
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-07-06 09:16:14 -0300

    conversor para departamento

---
+++ src/main/java/com/br/gti/sistemagti/web/controller/conversor/StringToDepartamentoConverter.java
@@ -0,0 +1,23 @@
+package com.br.gti.sistemagti.web.controller.conversor;
+
+import com.br.gti.sistemagti.domain.Departamento;
+import com.br.gti.sistemagti.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToDepartamentoConverter implements Converter<String, Departamento> {
+
+    @Autowired
+    private DepartamentoService service;
+
+    @Override
+    public Departamento convert(String text) {
+        if(text.isEmpty()){
+            return null;
+        }
+        Long id = Long.valueOf(text);
+        return service.buscarPorId(id);
+    }
+}
