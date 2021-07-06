a6e84f49a46af1ebc411471fb24b8de902d28541
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-07-06 09:57:52 -0300

    listando funcionários

--- src/main/java/com/br/gti/sistemagti/web/controller/FuncionarioController.java
+++ src/main/java/com/br/gti/sistemagti/web/controller/FuncionarioController.java
@@ -6,6 +6,7 @@ import com.br.gti.sistemagti.service.DepartamentoService;
 import com.br.gti.sistemagti.service.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +31,8 @@ public class FuncionarioController {
     }
 
     @GetMapping("/listar")
-    public String listar(){
+    public String listar(ModelMap model){
+        model.addAttribute("funcionarios",funcionarioService.buscarTodos());
         return "/funcionario/lista";
     }
 
