61ebd15ee453b5b4ed29fdf8b3e89cae0594a6c1
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-08-09 23:40:11 -0300

    invalidando sessão do primeiro login para logar em um novo dispositivo

--- src/main/java/com/br/gti/sistemagti/config/SecurityConfig.java
+++ src/main/java/com/br/gti/sistemagti/config/SecurityConfig.java
@@ -3,11 +3,16 @@ package com.br.gti.sistemagti.config;
 import com.br.gti.sistemagti.domain.PerfilTipo;
 import com.br.gti.sistemagti.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //acessos públicos liberados
                 .antMatchers("/webjars/**", "/css/**", "/js/**", "/image/**").permitAll()
                 .antMatchers("/u/e/**").permitAll()
+                .antMatchers("/expired").permitAll()
 
                 //acessos privados admin
                 .antMatchers("/u/editar/senha", "/u/confirmar/senha").hasAuthority(ESTAGIARIO)
@@ -47,15 +53,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .logout()
                     .logoutSuccessUrl("/login")
+                .deleteCookies("JSESSIONID")
                 .and()
                     .exceptionHandling()
                     .accessDeniedPage("/acesso-negado")
                 .and()
                     .rememberMe();
+
+        http.sessionManagement()
+                .maximumSessions(1)
+                .expiredUrl("/expired")
+                .maxSessionsPreventsLogin(false)
+                .sessionRegistry(sessionRegistry());
+
+        http.sessionManagement()
+                .sessionFixation().newSession()
+                .sessionAuthenticationStrategy(sesseionAuthStrategy());
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public SessionAuthenticationStrategy sesseionAuthStrategy(){
+        return  new RegisterSessionAuthenticationStrategy(sessionRegistry());
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
     }
 }
--- src/main/java/com/br/gti/sistemagti/web/controller/LoginController.java
+++ src/main/java/com/br/gti/sistemagti/web/controller/LoginController.java
@@ -18,8 +18,8 @@ public class LoginController {
     public String loginError(ModelMap model) {
         model.addAttribute("alerta", "erro");
         model.addAttribute("titulo", "Credenciais inválidas");
-        model.addAttribute("texto", "Login ou senha incorretos, tente novamente,");
-        model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados");
+        model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+        model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
         return "login";
     }
 
@@ -29,6 +29,15 @@ public class LoginController {
         model.addAttribute("error", "Acesso negado");
         model.addAttribute("message", "Você não tem permissão para acesso a esta área ou ação.");
         return "error";
+    }
+
+    @GetMapping("/expired")
+    public String sessaoExpirada(ModelMap model) {
+        model.addAttribute("alerta", "erro");
+        model.addAttribute("titulo", "Acesso recusado!");
+        model.addAttribute("texto", "Sua sessão expirou");
+        model.addAttribute("subtexto", "Usuário logado em outro dispositivo");
+        return "login";
     }
 
 
